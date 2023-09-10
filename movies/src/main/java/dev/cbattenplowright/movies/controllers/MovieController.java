7eab8ad4389c1af6fd1187c2de5da03413a835cc
Callum BP <callumbp15@gmail.com>
2023-09-10 22:46:10 +0100

    INDEX endpoint added

--- movies/src/main/java/dev/cbattenplowright/movies/controllers/MovieController.java
+++ movies/src/main/java/dev/cbattenplowright/movies/controllers/MovieController.java
@@ -1,18 +1,26 @@
 package dev.cbattenplowright.movies.controllers;
 
+import dev.cbattenplowright.movies.models.Movie;
+import dev.cbattenplowright.movies.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping
-    public ResponseEntity<String> allMovies() {
-        return new ResponseEntity<>("All Movies", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
 }
