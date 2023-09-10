03c48109371731ba127ea40956360c96989f88a2
Callum BP <callumbp15@gmail.com>
2023-09-10 22:29:46 +0100

    created movie and review models

--- movies/src/main/java/dev/cbattenplowright/movies/controllers/MovieController.java
+++ movies/src/main/java/dev/cbattenplowright/movies/controllers/MovieController.java
@@ -1,17 +1,18 @@
 package dev.cbattenplowright.movies.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("movies")
+@RequestMapping("/api/v1/movies")
 public class MovieController {
 
     @GetMapping
-    public String apiRoute() {
-        return "Hello World!";
+    public ResponseEntity<String> allMovies() {
+        return new ResponseEntity<>("All Movies", HttpStatus.OK);
     }
 
 }
