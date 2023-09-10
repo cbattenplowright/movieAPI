47fa1da6729ba1abc75ff538c9b42b19f77ac352
Callum BP <callumbp15@gmail.com>
2023-09-10 22:32:06 +0100

    created repository

---
+++ movies/src/main/java/dev/cbattenplowright/movies/repositories/MovieRepository.java
@@ -0,0 +1,10 @@
+package dev.cbattenplowright.movies.repositories;
+
+import dev.cbattenplowright.movies.models.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+}
