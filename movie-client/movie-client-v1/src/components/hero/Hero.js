134e55e409f77025626f79ab630ec56f5e630c5c
Callum BP <callumbp15@gmail.com>
2023-09-12 00:48:09 +0100

    added styling to the Hero component

--- movie-client/movie-client-v1/src/components/hero/Hero.js
+++ movie-client/movie-client-v1/src/components/hero/Hero.js
@@ -10,15 +10,20 @@ const Hero = ({ movies }) => {
         return (
             <Paper key={movie.id}>
                 <div className="movie-card-container">
-                    <div className="movie-detail">
-                        <div className="movie-poster">
-                            <img
-                                src={movie.poster}
-                                alt=""
-                            />
-                        </div>
-                        <div className="movie-title">
-                            <h4>{movie.title}</h4>
+                    <div
+                        className="movie-card"
+                        style={{ "--img": `url(${movie.backdrops[0]})` }}
+                    >
+                        <div className="movie-detail">
+                            <div className="movie-poster">
+                                <img
+                                    src={movie.poster}
+                                    alt=""
+                                />
+                            </div>
+                            <div className="movie-title">
+                                <h4>{movie.title}</h4>
+                            </div>
                         </div>
                     </div>
                 </div>
