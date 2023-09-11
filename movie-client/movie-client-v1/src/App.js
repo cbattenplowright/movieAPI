e110c91e6ceb346502dc3f67a482f5b11650d315
Callum BP <callumbp15@gmail.com>
2023-09-11 22:35:31 +0100

    getMovies function written

--- movie-client/movie-client-v1/src/App.js
+++ movie-client/movie-client-v1/src/App.js
@@ -1,25 +1,28 @@
-import './App.css';
-import axiosConfig from './API/axiosConfig';
+import "./App.css";
+import api from "./API/axiosConfig";
+import { useState, useEffect } from "react";
+import { json } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [movies, setMovies] = useState();
+
+    const getMovies = async () => {
+        try {
+            const response = await api.get("/api/v1/movies");
+            const jsonData = await response.json();
+
+            console.log(jsonData);
+            setMovies(jsonData);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    return <div className="App"></div>;
 }
 
 export default App;
